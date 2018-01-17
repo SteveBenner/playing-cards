c3cec7389dc1d87a9b484e472c6e850b89b97f31
SteveBenner <sbenner9@gmail.com>
2018-01-17 10:55:13 -0800

    replace tabs with spaces

--- lib/pile.rb
+++ lib/pile.rb
@@ -1,23 +1,23 @@
 # A Pile is a generic collection of <Card> objects, super class to <Stack> and <Queue>.
 class Pile < Array
-	# For our purposes, shuffling should always be in place
-	alias :shuffle :shuffle!
+  # For our purposes, shuffling should always be in place
+  alias :shuffle :shuffle!
 end
 
 # Include LIFO or FIFO within a subclass of <Pile> to specify how members are accessed
 module LIFO
-	def self.included(base)
-		base.class_eval do
-			alias_method :push, :unshift
-			alias_method :pop, :shift
-		end
-	end
+  def self.included(base)
+    base.class_eval do
+      alias_method :push, :unshift
+      alias_method :pop, :shift
+    end
+  end
 end
 
 module FIFO
-	def self.included(base)
-		base.class_eval do
-			alias_method :pop, :shift
-		end
-	end
+  def self.included(base)
+    base.class_eval do
+      alias_method :pop, :shift
+    end
+  end
 end
